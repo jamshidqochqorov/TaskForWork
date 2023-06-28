e24cb36580b0529911ba84ee11db1a6caf5b1909
jamshid <jamshidqochqorov84@gmail.com>
2023-06-28 20:48:56 +0500

    added Seeder Model Migrate files

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'password'=>Hash::make('secret'),
+            'role_id'=>1
+        ]);
+    }
+}
